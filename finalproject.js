0d88718d295f7833d4d53f5a60c30c795de0d074
EthanMoreta <110928975+EthanMoreta@users.noreply.github.com>
2022-11-30 17:31:44 -0500

    Update finalproject.js

--- finalproject.js
+++ finalproject.js
@@ -197,7 +197,106 @@ fill(255, 255, 0);
 ellipse(x-size*27.5/151, y+size*102/151, size*10/151, size*10/151);
 ellipse(x+size*27.5/151, y+size*102/151, size*10/151, size*10/151);
 };
+//Mario
+var drawMarioHead = function(bitmojiX,bitmojiY,h)   
+{
+    var p=h/149;
+    fill(219, 174, 107);//skin tone
+    rect(bitmojiX-9*p,bitmojiY+23*p,21*p,64*p);
+    fill(219, 174, 107); //ear color
+    arc(bitmojiX-36*p,bitmojiY+5*p,23*p,28*p,-1*p,373*p);//left ear
+    arc(bitmojiX+33*p,bitmojiY+3*p,23*p,28*p,-1*p,381*p);//right ear
+    ellipse(bitmojiX+2*p,bitmojiY+2*p,104*p,148*p);//head
+};
+var drawMarioGlasses = function(bitmojiX,bitmojiY,h) 
+{
+    var p=h/100;
+    fill(255, 255, 255);//brown eyes
+    ellipse(bitmojiX-10*p,bitmojiY-0*p,15*p,14*p);//left eye
+    ellipse(bitmojiX+16*p,bitmojiY-0*p,15*p,14*p);//right eye
+    fill(0, 242, 255);
+    ellipse(bitmojiX-10*p,bitmojiY+0*p,5*p,8*p);//brownleft pupil
+    ellipse(bitmojiX+16*p,bitmojiY+0*p,5*p,8*p);//brownright pupil 
+};
+var drawMarioFaceDetails = function(bitmojiX,bitmojiY,h) 
+{
+    var p=h/100;
+    fill(0, 0, 0);
+    ellipse(bitmojiX-10*p,bitmojiY-0*p,2*p,3*p);//left pupil
+    ellipse(bitmojiX+16*p,bitmojiY-0*p,2*p,3*p);//right pupil
+    fill(219, 174, 107); //nose
+    bezier(bitmojiX-1*p,bitmojiY+1*p,bitmojiX+21*p,bitmojiY+22*p,bitmojiX-8*p,bitmojiY+20*p,bitmojiX-4*p,bitmojiY+15*p);//nose
+    //arc(bitmojiX+3*p,bitmojiY+28*p,20*p,8*p,1*p,180*p);//bottomlip
+    
+    noFill();
+    arc(bitmojiX+-10*p,bitmojiY+-5*p,19*p,16*p,182,360);//eyebrow
+    arc(bitmojiX+15*p,bitmojiY+-5*p,19*p,16*p,182,360);//eyebrow
 
+    //fill(168, 110, 59);
+    //arc(bitmojiX+3*p,bitmojiY+27*p,30*p,-8*p,-6,188);//toplip
+    //fill(0, 0, 0);
+    noFill();
+    arc(bitmojiX+0*p,bitmojiY+30*p,36*p,-4*p,-188,5);//mouth
+    //line(bitmojiX-12*p,bitmojiY+26*p,bitmojiX+17*p,bitmojiY+27*p);//line between mouth
+    fill(69, 53, 11);
+    noStroke();
+    //arc(bitmojiX+3*p,bitmojiY+23*p,40*p,-8*p,124,408);//beardtop
+    arc(bitmojiX+-15*p,bitmojiY+19*p,19*p,-15*p,182,360);//beardtop
+    arc(bitmojiX+1*p,bitmojiY+19*p,19*p,-15*p,182,360);//beardtop
+    arc(bitmojiX+16*p,bitmojiY+19*p,19*p,-15*p,182,360);//beardtop
+
+};
+var drawMarioHair = function(bitmojiX,bitmojiY,h)
+{
+    var p=h/100;
+    fill(74, 54, 7); //brown hair
+    noStroke();
+    ellipse(bitmojiX-28*p,bitmojiY-9*p,14*p,27*p);//1 hair
+    //ellipse(bitmojiX-12*p,bitmojiY-44*p,31*p,27*p);//2 hair
+    //ellipse(bitmojiX+11*p,bitmojiY-45*p,31*p,19*p);//3 hair
+    ellipse(bitmojiX+32*p,bitmojiY-9*p,14*p,26*p);//4 hair
+    stroke(0,0,0);//black glasses
+};
+var drawMarioHat = function (bitmojiX,bitmojiY,h)
+{
+    var p=h/100;
+    fill(255, 0, 0);
+    noStroke();
+    ellipse(bitmojiX+1*p,bitmojiY-24*p,93*p,23*p); //hat 
+    ellipse(bitmojiX+2*p,bitmojiY-40*p,65*p,52*p); //tophat 
+    fill(255, 255, 255);
+    textFont(createFont("TIMES",(30*p)));
+    text("M",bitmojiX+-12*p,bitmojiY+-36*p);
+
+};
+var drawMarioBody = function(bitmojiX,bitmojiY,h)
+{
+    var p=h/100;
+    rectMode(CORNER);
+    fill(219, 174, 107);//armscolor
+    rect(bitmojiX-53*p,bitmojiY+70*p,106*p,38*p);//arms
+    fill(255, 0, 0);
+    arc(bitmojiX-39*p,bitmojiY+65*p,30*p,44*p,-490*p,-23*p);//left shoulder
+    arc(bitmojiX+41*p,bitmojiY+65*p,30*p,43*p,-490*p,75*p);//right shoulder
+    fill(0, 81, 255);//shirt color
+    rect(bitmojiX-47*p,bitmojiY+57*p,95*p,55*p);//shirt
+    fill(219, 174, 107);
+    arc(bitmojiX+0*p,bitmojiY+57*p,22*p,15*p,0*p,180*p);//neckinshirt
+    fill(255, 238, 0);
+    ellipse(bitmojiX+19*p,bitmojiY+80*p,12*p,14*p); //Yellow Buttons on shirt 
+    ellipse(bitmojiX+-11*p,bitmojiY+80*p,12*p,14*p); //Yellow Buttons on shirt 
+
+    fill(0, 0, 0);
+};
+var drawMario = function(bitmojiX,bitmojiY,h)
+{
+drawMarioHead(bitmojiX,bitmojiY,h);
+drawMarioGlasses(bitmojiX,bitmojiY,h);
+drawMarioFaceDetails(bitmojiX,bitmojiY,h);
+drawMarioHair(bitmojiX,bitmojiY,h);
+drawMarioBody(bitmojiX,bitmojiY,h);
+drawMarioHat(bitmojiX,bitmojiY,h);
+};
 
 
 
@@ -229,7 +328,7 @@ var start = new Button(150, 315, 100, 50, color(0, 115, 94), "START");
 var drawScene0 = function() {
     background(101, 0, 184);
     drawBitmoji(135, 225, 75);
-    drawEthansBitmoji(265, 225, 44);
+    drawEthansBitmoji(268, 225, 44);
     textFont(sm256);
     textAlign(CENTER, CENTER);
     textSize(14);
@@ -242,12 +341,28 @@ var drawScene1 = function() {
     background(0, 106, 138);
     fill(255, 255, 255);
     textSize(34);
-    text("Character select", 200, 20);
+    text("Character select", 85, 25);
     stroke(255, 255, 255);
     fill(0, 0, 0);
     for (var i = 0; i < 4; i++) {
         rect(i*100 + 5, 150, 90, 90);
     }
+    stroke(0, 0, 0);
+    drawLuigi(49,186,66);
+    drawMario(149,185,49);
+    stroke(0, 0, 0);
+    drawBitmoji(251,186,66);
+    stroke(0, 0, 0);
+    drawEthansBitmoji(350,177,45);
+    fill(255, 255, 255);
+    text("Mario", 128,3, 500,500);
+    text("Luigi", 29,3, 500,500);
+    text("Ryan", 229,3, 500,500);
+    text("Ethan", 330,3, 500,500);
+};
+
+var drawScene2 = function() {
+    
 };
 
 draw = function() {
