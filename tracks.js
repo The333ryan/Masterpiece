0cc9c1f13add8474218f8db04c15e9eaa41411d0
EthanMoreta <110928975+EthanMoreta@users.noreply.github.com>
2022-12-14 16:58:11 -0500

    Create tracks.js

---
+++ tracks.js
@@ -0,0 +1,50 @@
+var drawSquareCircuit = function(){
+    background(0, 143, 43);
+    fill(148, 143, 143);
+    rect(47,63,298,272,9);
+    fill(0, 143, 43);
+    rect(107,117,175,172,9);
+    fill(247, 255, 0);
+    rect(145,64,13,65);
+    
+};
+
+
+var drawCircleCircuit = function(){
+    background(0, 143, 43); 
+    fill(148, 143, 143);    
+    ellipse(197,201,342,372);   
+    fill(0, 143, 43);   
+    ellipse(194,204,227,263); 
+    fill(247, 255, 0); 
+    rect(185,13,10,59);  
+    
+};
+
+
+var drawTraingleCircuit = function(){
+    background(0, 143, 43);
+    fill(148, 143, 143);
+    triangle(42,328,373,328,186,33);
+    fill(0, 143, 43);
+    triangle(282,560/2,237/2,560/2,382/2,123);
+    fill(247, 255, 0);
+    quad(174,158,180,147,149,111,142,123);
+    
+};
+
+
+var drawRyansCircuit = function(){
+    background(0, 143, 43);
+    fill(148, 143, 143);
+    rect(47,63,75,272);
+    rect(47,63,298,64);
+    quad(215, 227, 295, 228, 345, 127, 267, 128);
+    quad(296, 225, 374, 330, 297, 327, 216, 226);
+    quad(258, 278, 293, 321, 122, 306, 122, 244);
+    fill(255, 238, 0);
+    rect(47,181,75,14);
+
+};
+
+
