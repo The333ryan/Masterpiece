ada53b54085a8c54f470b6333220f064dc3d81e8
EthanMoreta <110928975+EthanMoreta@users.noreply.github.com>
2022-12-14 17:47:31 -0500

    Update tracks.js

--- tracks.js
+++ tracks.js
@@ -5,8 +5,12 @@ var drawSquareCircuit = function(){
     fill(0, 143, 43);
     rect(107,117,175,172,9);
     fill(247, 255, 0);
-    rect(145,64,13,65);
-    
+    rect(113,64,13,53);
+    fill(0, 225, 255);
+    noStroke();
+    rect(254,64,13,53);  //checkpoint
+    rect(284,264,61,13);  //checkpoint
+    rect(113,290,13,45);  //checkpoint
 };
 
 
@@ -18,7 +22,10 @@ var drawCircleCircuit = function(){
     ellipse(194,204,227,263); 
     fill(247, 255, 0); 
     rect(185,13,10,59);  
-    
+    fill(0, 225, 255);
+    noStroke();
+    quad(292,272,340,300,347,285,298,257);
+    quad(46,286,56,303,105,285,95,270);
 };
 
 
@@ -30,7 +37,10 @@ var drawTraingleCircuit = function(){
     triangle(282,560/2,237/2,560/2,382/2,123);
     fill(247, 255, 0);
     quad(174,158,180,147,149,111,142,123);
-    
+    fill(0, 225, 255);
+    noStroke();
+    quad(278,272,338,272,329,259,271,259);
+    rect(126,281,13,46);
 };
 
 
@@ -44,6 +54,10 @@ var drawRyansCircuit = function(){
     quad(258, 278, 293, 321, 122, 306, 122, 244);
     fill(255, 238, 0);
     rect(47,181,75,14);
+    fill(0, 225, 255);
+    noStroke();
+    rect(252,64,13,63);
+    quad(196,311,214,267,200,264,182,310);
 
 };
 
